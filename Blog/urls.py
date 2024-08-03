bfe85354d0a94d9269cc430cc6b1afd01b19155b
Sherdos <sherdos26@gmail.com>
2024-08-03 19:55:31 +0600

    about

--- Blog/urls.py
+++ Blog/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from post.views import index, about, show_post
+from post.views import index, about, show_post,add_post
 from user.views import user_register,user_login, user_logout
 from django.contrib.auth.views import LogoutView
 
@@ -25,6 +25,7 @@ urlpatterns = [
     path('', index, name='home'),
     path('about/', about, name='about'),
     path('post/show/<int:id>/', show_post, name = 'show_post'),
+    path('post/add/', add_post, name = 'add_post'),
     path('user/register/', user_register, name='register'),
     path('user/login/',user_login, name='login'),
     path('user/logout/',user_logout, name='logout'),
--- post/views.py
+++ post/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
 from post.models import Post
 # Create your views here.
 # MVT - model veiw template
@@ -15,3 +15,10 @@ def about(request):
 def show_post(request, id):
     card = Post.objects.get(id=id)
     return render(request,'show_post.html', {'post':card})
+
+def add_post(request):
+    title = request.POST.get('title')
+    description = request.POST.get('description')
+    image = request.FILES.get('image')
+    Post.objects.create(title=title, desciption=description, image=image, user_id=request.user)
+    return redirect('home')
